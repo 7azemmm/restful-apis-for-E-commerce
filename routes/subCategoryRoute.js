9bfe64e797c3617bd38757ae9c1093f2b7753255
hazem mohamed <hazemm7622@gmail.com>
2023-05-31 16:42:13 +0200

    delete comments

--- routes/subCategoryRoute.js
+++ routes/subCategoryRoute.js
@@ -1,24 +1,3 @@
-/*const express = require ('express')
-
-const{createsubCategoryValidator,getsubCategoryValidator}=require('../utils/validators/subCategoryValidators');
-
-
-const{createSubCategory,
-    getsubCategory,
-    getsubCategories
-}=require('../services/subCategoryService');
-
-
-const router=express.Router();
-
-
-router.route('/').post(createsubCategoryValidator,createSubCategory)//.get(getsubCategories)
-router.route('/').get(getsubCategories);
-//router.route('/:id').get(getsubCategoryValidator,getsubCategory);
-
-
-
-module.exports=router;*/
 
 const express = require('express');
 
